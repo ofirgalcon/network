141f8cb56835ad5b7e20912748e15e6c50d831a4
tuxudo <tuxudo@users.noreply.github.com>
2023-03-29 13:30:38 -0400

    Update network_controller.php

--- network_controller.php
+++ network_controller.php
@@ -41,7 +41,6 @@ class Network_controller extends Module_controller
      **/
     public function routers()
     {
-        
         if (! $this->authorized()) {
             die('Authenticate first.'); // Todo: return json?
         }
@@ -111,6 +110,9 @@ class Network_controller extends Module_controller
      **/
     public function get_tab_data($serial_number = '')
     {
+        // Remove non-serial number characters
+        $serial_number = preg_replace("/[^A-Za-z0-9_\-]]/", '', $serial_number);
+
         $obj = new View();
 
         if (! $this->authorized()) {
